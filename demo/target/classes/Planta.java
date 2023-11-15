7b6f71d23827cc94f5a64923d6671ec7ebfbc2f9
merardo <merardo@gmail.com>
2023-11-15 15:17:14 +0100

    trabajando en el proyecto

---
+++ demo/target/classes/Planta.java
@@ -0,0 +1,35 @@
+package Planta;
+
+public class Planta {
+    private String Nombre;
+    private String Tipo;
+    private list <Cuidados> Cuidados;
+
+
+    public Planta (String Nombre, String Tipo){
+        this.Nombre = Nombre;
+        this.Tipo = Tipo;
+        this.<Cuidados> Cuidados = <Cuidados> Cuidados;
+
+    }
+
+    public String getNombre(){
+        return Nombre;
+
+    }
+
+    public void setNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+    
+
+    public String getTipo(){
+        return Nombre;
+
+    }
+
+    public void setTipo(String Tipo){
+        this.Tipo = Tipo;
+    }
+    
+}
