d2803c476f3017309fcc1b35e6f462e747df9968
merardo <merardo@gmail.com>
2023-11-16 13:30:28 +0100

    jejejej

---
+++ demo/target/classes/Area.java
@@ -0,0 +1,54 @@
+package Area;
+
+
+
+public class Area {
+
+    private String NombreA;
+    private String Ubicacion;
+    
+
+
+    public Botanico (String NombreA, String Ubicacion){
+        this.NombreA = NombreA;
+        this.Ubicacion = Ubicacion;
+      
+
+    }
+
+    public String getNombreA(){
+        return NombreA;
+
+    }
+
+    public void setNombreA(String NombreA){
+        this.NombreA = NombreA;
+    }
+    
+
+    public String getUbicacion(){
+        return Ubicacion;
+
+    }
+
+    public void setUbicacion(String Ubicacion){
+        this.Ubicacion = Ubicacion;
+
+    }
+  
+    
+   
+    @Override
+
+    public String toString (){
+        return Super.toString()+"NombreA"+NombreA+ ",Nombre"+Ubicacion;
+
+    }
+
+    
+
+
+}
+  
+    
+}
